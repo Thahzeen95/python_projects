3a4be3bdf03703461bdfdae289d657e9584d770d
Thahzeen95 <thahzeenas@gmail.com>
2025-12-09 16:08:08 +0530

    triviagame.py file created

---
+++ Practice_theory/type.py
@@ -0,0 +1,15 @@
+a="Thahzeen"
+b=30
+c=1995.10
+d=("I", "am", "Thahzeen")
+e=["I", "am", "Thahzeen"]
+f={"I", "am", "Thahzeen"}
+g={"I":10, "am":27, "Thahzeen":1995}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
---
+++ string_replace.py
@@ -0,0 +1,7 @@
+def string_replace(text,ch):
+    return text.replace(" ", ch)
+
+text="D t C mpBl ckFrid yS le"
+ch="a"
+
+string_replace(text,ch)
