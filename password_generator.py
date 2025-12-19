e8550a900deb4284671b9f3ead6e4b9c3939a41a
Thahzeen95 <thahzeenas@gmail.com>
2025-12-19 11:16:56 +0530

    Planning

---
+++ password_generator.py
@@ -0,0 +1,9 @@
+#Collect user details
+#length
+#Must have Uppercase
+#Must have digits
+#Must have special charecter
+#get all available charecters
+#randomly pick charecters
+#Ensure atleaste we one charecter of each type
+#Ensure length is valid
