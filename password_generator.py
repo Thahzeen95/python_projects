d09a6ae89c05a800852e84eb458fa813a7a10e8b
Thahzeen95 <thahzeenas@gmail.com>
2025-12-19 13:35:59 +0530

    Collect user details

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,20 @@
+import random
+import string
+
 #Collect user details
 #length
 #Must have Uppercase
 #Must have digits
 #Must have special charecter
+
+def generate_password():
+    length = int(input("Enter desired password length:").strip())
+    include_uppercase = input("Include upper case letters? (yes/no):").strip().lower()
+    include_digits = input("Include digits? (yes/no)").strip().lower()
+    include_special = input("Include special charecters? (yes/no)").strip().lower()
 #get all available charecters
 #randomly pick charecters
 #Ensure atleaste we one charecter of each type
 #Ensure length is valid
+
+generate_password()
